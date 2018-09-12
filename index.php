c1dc6f41b5d0102da6855540960fffad0816e303
Sacred <obruyako@gmail.com>
2018-09-12 22:27:32 +0300

    Test project

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+define ('PROJECT_DIR', __DIR__);
+define ('LAYOUT_DIR', PROJECT_DIR.'/layout');
+define ('CONTROLLER_DIR', PROJECT_DIR.'/controllers');
+
+require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/config.php';
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+\App\Core::getInstance()->setEntityManager($entityManager);
+
+\App\Front::run();
