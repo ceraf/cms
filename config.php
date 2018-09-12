e3f975d936a07960ead84f8a98ee83fc53d77c5d
Sacred <obruyako@gmail.com>
2018-09-12 23:17:13 +0300

    CMS

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => '12345',
+    'dbname'   => 'test_task',
+);
+
+$paths = array("/path/to/entity-files");
+$isDevMode = false;
